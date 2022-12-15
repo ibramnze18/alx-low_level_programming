2f7f3f2875f384507ba004643b16b2b32c566330
ibramnze18 <ibramnze18@gmail.com>
2022-12-14 18:59:09 -0800

    function task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,25 +5,32 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int r, c, d;
 
-	for (num = 0; num <= 9; num++)
+	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
-
-		for (mult = 1; mult <= 0; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
+		_putchar(',');
+		_putchar(' ');
+	for (c = 1; c <= 9; c++)
+	{
+		d = (r * c);
+	if ((d / 10) > 0)
+	{
+		_putchar((d / 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+	}
+		_putchar((d % 10) + '0');
+	if (c < 9)
+	{
+		_putchar(',');
+	    _putchar(' ');
+	}
+	}
+	_putchar('\n');
 	}
 }
+
