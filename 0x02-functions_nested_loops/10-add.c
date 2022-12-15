001f3c695d0dda1e8640cd2e92414557d57b6503
ibramnze18 <ibramnze18@gmail.com>
2022-12-14 19:08:38 -0800

    function task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function that sums
+ * @a: An integer a
+ * @b: An integer b
+ * Description: The sum of two integers
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
