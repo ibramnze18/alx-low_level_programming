0f728e7aa9fe56b9afb882092eebe733cbd414fe
ibramnze18 <ibramnze18@gmail.com>
2022-12-19 14:00:02 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
  */
 void print_rev(char *s)
 {
-	int num = 0;
+	int count = 0;
 
-	while (num >= 0)
+	while (count >= 0)
 	{
-		if (s[num] == '\0')
+		if (s[count] == '\0')
 			break;
-		num++;
+		count++;
 	}
 	for (count--; count >= 0; count--)
 		_putchar(s[count]);
