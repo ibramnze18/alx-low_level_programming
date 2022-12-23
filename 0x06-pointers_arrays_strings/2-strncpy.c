7c18b557965b5515470190d4f10cf3c83911afb5
ibramnze18 <ibramnze18@gmail.com>
2022-12-23 15:26:02 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string n
+ * @dest: destination character
+ * @src: source character
+ * @n: number of character to copy
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] != '\0';
+		j++;
+	}
+	return (dest);
+
+}
