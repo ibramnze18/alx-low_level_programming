1bd9741b9c48d95adaa35b5ce6062030b95823a7
ibramnze18 <ibramnze18@gmail.com>
2023-01-09 08:06:43 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Function containing argc and argv
+ * @argc: Argument counter
+ * @argv: Argument variable
+ * Return: Always 0 On Success
+ */
+
+int main(argc__attribute__((unused)), argv[])
+{
+	_putchar("%s\n", argv[0]);
+}
