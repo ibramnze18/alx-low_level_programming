d087bb85d4a38daf2cc7592ee1ebafc7ff259015
ibramnze18 <ibramnze18@gmail.com>
2023-01-10 12:48:29 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
+ * check_count - converts a string to an integer
  * @s: string to be converted
- * Return: the int converted from the string
+ * Return: the integer converted from the string
  */
-int _atoi(char *s)
+int check_count(char *s)
 {
 	int i, d, n, len, f, digit;
 
