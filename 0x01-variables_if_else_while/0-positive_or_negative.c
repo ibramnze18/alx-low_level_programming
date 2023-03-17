695ea68623acbd89d63de2190ce5de364ce749b7
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-03-17 09:02:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,7 @@
 
 /**
  * main - Main Entry point
- * 
- * Return: Always 0 (Success)  
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,11 +12,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
