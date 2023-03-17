9498c80592c53d65b4de50183e3257ff05595797
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-03-17 14:53:25 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,12 @@
  * main - Prints the alphabet.
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
 
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alphabets[i]);
