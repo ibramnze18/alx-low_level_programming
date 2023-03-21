ddc61d51c879b50cdd9426910eef573354188076
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-03-21 13:20:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * print_alphabet Function to print alphabets a-z
+ * print_alphabet - Function to print alphabets a-z
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void)
-	{
-		char ch;
+	char ch;
 
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
-	}
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
 	_putchar('\n');
-	return (0);
 }
