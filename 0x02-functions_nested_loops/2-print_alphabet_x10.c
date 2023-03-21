986a44521daa7609ddd2964eeb797570af7f787d
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-03-21 13:31:50 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10X the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
