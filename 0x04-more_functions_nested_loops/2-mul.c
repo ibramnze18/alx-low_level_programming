41f54a3321938c11030546996ff9d6ae2717b163
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-03-23 07:40:45 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ * mul - Function that multiply two numbers
+ * @a: First operand
+ * @b: Second operand
+ * Return: Returns the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
