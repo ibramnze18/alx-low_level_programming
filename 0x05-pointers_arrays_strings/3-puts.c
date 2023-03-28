1496518b7b9efbcb508696fea650d6b8d522a5bf
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-03-28 12:51:33 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a string followed by a newline
+ * @str: String to print
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int i;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
