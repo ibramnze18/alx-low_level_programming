0d671e438c8b885a740eef84bc6d52aa90b516ee
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-05-03 01:44:44 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,19 +3,19 @@
 /**
  * print_listint - Function that prints all the elements of a linked list
  * @h: Head pointer
- *
  * Return: Number of all nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t numberOfSize = 0;
+	size_t n;
 
-	while (h)
+	n = 0;
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		numberOfSize++;
 		h = h->next;
+		n++;
 	}
 
-	return (numberOfSize);
+	return (n);
 }
