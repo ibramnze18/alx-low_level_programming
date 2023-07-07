cc6dacb207f767beccfcae5c14e5ad891f359731
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-07-07 08:29:55 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit -  A functn dat returns the value of a bit at a given index
+ * @n: To search number
+ * @index: index of number to search
+ *
+ * Return: the value of the bit at index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
+}
